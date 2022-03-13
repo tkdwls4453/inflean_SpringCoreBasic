be783db7ee90053b500359f266b76c131744c040
SANGJIN YOON <tkdwls734@gmail.com>
2022-03-13 21:47:16 +0900

    스프링 핵심 원리 이해2

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,7 +14,7 @@ public class OrderServiceImpl implements OrderService{
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }
+    } 
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
